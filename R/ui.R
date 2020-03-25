a15b62858208d3ceac7a5f519b1acacda79b3e98
keripukki <arttu.kosonen@gmail.com>
2020-03-25 17:38:07 +0200

    Add time parameters

--- R/ui.R
+++ R/ui.R
@@ -7,9 +7,51 @@ ui <- dashboardPage(
   
   dashboardSidebar(
     
-    # TODO time parameters module
-    #   - Main time parameters
-    #   - A/B time parameters
+    tags$head(
+      tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+    ),
+    
+    # Time parameters.
+    # We may want to make this into a separate module
+    # if we implement A/B comparison features at some point.
+    dateRangeInput(
+      inputId = "daterange",
+      label = "Start and end (inclusive) dates",
+      start = "2019-11-01",
+      end = "2019-11-30",
+      min = "2019-11-01",
+      max = "2019-11-30",
+      format = "d.m.yyyy",
+      separator = "-",
+      width = "100%"
+    ),
+    
+    checkboxGroupButtons(
+      inputId = "weekdays",
+      label = "Weekdays",
+      choices = c(
+        "Mon" = 1,
+        "Tue" = 2,
+        "Wed" = 3,
+        "Thu" = 4,
+        "Fri" = 5,
+        "Sat" = 6,
+        "Sun" = 7
+      ),
+      selected = 1:7,
+      justified = TRUE
+    ),
+    
+    sliderInput(
+      inputId = "timerange",
+      label = "Start and end (exclusive) hours",
+      min = 0,
+      max = 24,
+      value = c(0, 24),
+      step = 1,
+      ticks = FALSE,
+      width = "100%"
+    ),
     
     # TODO select mode
     
